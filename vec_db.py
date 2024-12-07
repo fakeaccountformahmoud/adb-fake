2d43c0de4754a844509787a349823e0c0f6aeda8
fakeaccountformahmoud <mahmoud.khattab03@eng-st.cu.edu.eg>
2024-12-07 17:28:59 +0200

    Update vec_db.py

--- vec_db.py
+++ vec_db.py
@@ -127,7 +127,7 @@ class VecDB:
         
         if self._get_num_records() == 20000000:
             #print(top_k)
-            scores = best_centroids[:10]
+            scores = best_centroids[:top_k]
         elif self._get_num_records() == 15000000:
             scores = best_centroids[:40]
         elif self._get_num_records() == 10000000:
